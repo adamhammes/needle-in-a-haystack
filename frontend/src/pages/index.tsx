45c6dfd0208ff5d9dac95ddcd361429d1be21fa3
Adam Hammes <ahammes@cortexmedia.ca>
2020-08-30 13:30:53 -0400

    Allow uploading multiple images & downloading as a CSV

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react"
 
+import serializeToCSV from "csv-stringify/lib/sync"
+
 import "./index.scss"
-import { ImageList } from "../components/image-list"
 
 const lambdaUrl = "https://k3bth73v75.execute-api.us-east-1.amazonaws.com/dev"
 
@@ -16,6 +17,11 @@ interface ApiImages {
 interface ApiResponse {
   severity: number
   images: ApiImages
+}
+
+interface ImageResult extends ApiResponse {
+  originalImage: string
+  originalImageName: string
 }
 
 const fileToBase64 = async (file: File): Promise<string> => {
@@ -34,58 +40,81 @@ const fileToBase64 = async (file: File): Promise<string> => {
   })
 }
 
+const downloadCSV = (results: ImageResult[]) => {
+  const rows = [
+    ["Image Name", "Severity"],
+    ...results.map(result => [result.originalImageName, result.severity]),
+  ]
+
+  const csvContent = serializeToCSV(rows)
+  const csvDataURI = encodeURI("data:text/csv;charset=utf-8," + csvContent)
+  window.open(csvDataURI)
+}
+
 export default function Home() {
   const [selectedFiles, setSelectedFiles] = useState(
     undefined as undefined | FileList
   )
 
-  const [apiResponse, setApiResponse] = useState(
-    undefined as undefined | ApiResponse
-  )
-
-  console.log(selectedFiles)
+  const [results, setResults] = useState(undefined as undefined | ImageResult[])
 
   const uploadFiles = async () => {
     if (selectedFiles != null && selectedFiles.length > 0) {
-      const encodedFile = await fileToBase64(selectedFiles[0])
+      setResults(
+        await Promise.all(
+          [...selectedFiles].map(async file => {
+            const encodedFile = await fileToBase64(file)
+            const response = await fetch(lambdaUrl, {
+              method: "POST",
+              body: JSON.stringify({
+                image_name: file.name,
+                image_body: encodedFile,
+              }),
+            })
 
-      const response = await fetch(lambdaUrl, {
-        method: "POST",
-        body: JSON.stringify({
-          image_name: selectedFiles[0].name,
-          image_body: encodedFile,
-        }),
-      })
-
-      setApiResponse(await response.json())
+            const data: ApiResponse = await response.json()
+            return {
+              ...data,
+              originalImage: dataSrcPrefix + encodedFile,
+              originalImageName: file.name,
+            }
+          })
+        )
+      )
     }
   }
 
   return (
     <div className="app__container">
-      <h1>Upload an image</h1>
+      <h2>Upload your images</h2>
       <input
         type="file"
         onChange={event => setSelectedFiles(event.target.files)}
+        multiple={true}
       />
       <div className="app__upload-button-container">
         <button disabled={!selectedFiles} onClick={uploadFiles}>
           upload
         </button>
       </div>
-      {!!apiResponse && (
-        <div>
-          <div>Severity: {apiResponse.severity}</div>
-
-          <ImageList
-            images={Object.entries(apiResponse.images).map(
-              ([name, base64]) => ({
-                name,
-                src: dataSrcPrefix + base64,
-              })
-            )}
-          />
-        </div>
+      {!!results && (
+        <>
+          <h2>Results</h2>
+          <button onClick={() => downloadCSV(results)}>
+            Download as a CSV
+          </button>
+          <ul className="result__listing">
+            {results.map(result => (
+              <li key={result.originalImage} className="result__item">
+                <h3>{result.originalImageName}</h3>
+                Severity: {result.severity}
+                <div className="result__image-wrapper">
+                  <img className="result__image" src={result.originalImage} />
+                </div>
+              </li>
+            ))}
+          </ul>
+        </>
       )}
     </div>
   )
